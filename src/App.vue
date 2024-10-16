7d233d3d5472e3fad96643739df621d94e37bd6d
Zulfaabam <zulfafatahakbar@gmail.com>
2024-10-16 22:01:58 +0700

    wip: initiate pixel style & create header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import moment from 'moment';
+import Avatar from './components/Avatar.vue';
+
+const today = moment(new Date()).format('dddd, DD MMMM YYYY');
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Daily Quest</h1>
+    <p>{{ today }}</p>
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <Avatar />
+    <h2>Abam</h2>
+    <progress></progress>
+    <p>Level 1</p>
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* border: 1px solid red; */
 }
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    width: 1024px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  header h1 {
+    font-size: 64px;
+    font-weight: 500;
   }
 
-  header .wrapper {
+  header p {
+    font-size: 20px;
+  }
+
+  main {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 28px;
   }
 }
 </style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="avatar pixel-corners"></div>
+</template>
+
+<style>
+.avatar {
+  width: 170px;
+  height: 170px;
+  background-color: rgb(232, 232, 232);
+}
+</style>
