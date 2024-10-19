0874749322dd3cb76dd6f5fad809cba198070edc
Zulfaabam <zulfafatahakbar@gmail.com>
2024-10-19 13:19:39 +0700

    wip: floating nav shadow & animation

--- src/components/FloatingNav.vue
+++ src/components/FloatingNav.vue
@@ -16,8 +16,8 @@ const navItems = [
 </script>
 
 <template>
-  <nav class="floating-nav pixel-corners--dark-purple">
-    <ul>
+  <nav class="floating-nav">
+    <ul class="pixel-corners--dark-purple">
       <li v-for="item in navItems">
         <a :href="item.path">
           <img :src="item.icon" alt="icon" width="36" height="36" />
@@ -35,7 +35,8 @@ const navItems = [
   transform: translateX(-50%);
   width: 500px;
   height: 80px;
-  background-color: var(--gray);
+  filter: drop-shadow(0px 4px 4px rgb(232, 232, 232, 0.5));
+  animation: bounce 1s ease-in-out infinite;
 }
 
 .floating-nav ul {
@@ -43,5 +44,24 @@ const navItems = [
   justify-content: space-evenly;
   align-items: center;
   height: 100%;
+  background-color: var(--gray);
+}
+
+@keyframes bounce {
+  0% {
+    bottom: 48px;
+  }
+  25% {
+    bottom: 48.5px;
+  }
+  50% {
+    bottom: 49px;
+  }
+  75% {
+    bottom: 48.5px;
+  }
+  100% {
+    bottom: 48px;
+  }
 }
 </style>
